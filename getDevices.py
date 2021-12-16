f6f8528454d8824f2e8a10871f7c2f24ddeab4b7
Vytex0 <Vytex0@gmail.com>
2021-12-16 18:55:12 +0100

    add commentaries

--- getDevices.py
+++ getDevices.py
@@ -5,14 +5,15 @@ print("==========================")
 print("Listing des 'Devices' accessibles par l'utilisateur courant")
 print("==========================")
 
-print("Device n°",1,":","PC")
+# We get the list of device instances
+handles, names = win32pdh.EnumObjectItems(None, None, "Device", win32pdh.PERF_DETAIL_WIZARD)
 
-items, instances = win32pdh.EnumObjectItems(None, None, "Device", win32pdh.PERF_DETAIL_WIZARD)
+# We loop for all instances
+for i in range(len(names)):
+    name = names[i]
+    handle = handles[i]
 
-for i in range(len(instances)):
-    name = instances[i]
-    handle = items[i]
-
+    # The last parameter corresponds to the information that we want to get
     securityDescriptor_owner = win32security.GetKernelObjectSecurity(handle, 0)
     securityDescriptor_group = win32security.GetKernelObjectSecurity(handle, 1)
     securityDescriptor_dacl = win32security.GetKernelObjectSecurity(handle, 2)
